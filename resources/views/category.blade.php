bc4936c5e1103519b2cc8737a379467078f397fa
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-10-17 17:31:09 +0200

    subscribe

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -80,6 +80,7 @@
             </div>
         </div>
     </section>
+    @include('extensions.subscribe')
 @endsection
 @push('scripts')
     <script src="https://www.youtube.com/iframe_api"></script>
