e059bd44917e92c40ecc1028d3be017f011183f8
Abdallah Ahmed <abdallah.se95@gmail.com>
2018-10-24 16:01:40 +0200

    UI changes

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,26 +4,26 @@
     <div class="search-section">
         <section>
             @include('layouts.partials.header')
-        </section>
-        <div class="search-page">
-            <div class="container">
-                <form id="search">
-                    <input type="text" name="q" maxlength="50" placeholder="إبحث" class="third-title-font">
-                    <button name="search-submit">
-                        <i class="icon-arrow-left"></i>
-                    </button>
-                </form>
-                <img src="{{assets('assets')}}/images/red.png" alt="" class="red">
-            </div>
-        </div>
-        <section id="list">
-            <div class="container">
-                <img src="{{assets('assets')}}/images/green.png" alt="" class="green">
-                <div class="cards">
-                    @include('extensions.index-videos', $videos)
-                    <div id="more"></div>
+                <div class="search-page">
+                    <div class="container">
+                        <form id="search">
+                            <input type="text" name="q" maxlength="50" placeholder="إبحث" class="third-title-font">
+                            <button name="search-submit">
+                                <i class="icon-arrow-left"></i>
+                            </button>
+                        </form>
+                        <img src="{{assets('assets')}}/images/red.png" alt="" class="red">
+                    </div>
                 </div>
-            </div>
+                <div id="list">
+                    <div class="container">
+                        <img src="{{assets('assets')}}/images/green.png" alt="" class="green">
+                        <div class="cards">
+                            @include('extensions.index-videos', $videos)
+                            <div id="more"></div>
+                        </div>
+                    </div>
+                </div>
         </section>
         <div id="scroll-to"></div>
     </div>
